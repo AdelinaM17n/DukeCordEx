1b064a26f91f1c9b27fba295c35b4dce35ad2aa8
Maheesha <maheeshasithum@gmail.com>
2022-06-25 14:28:57 +0530

    Modularization preparation for subcommands and groups

--- src/main/java/io/github/maheevil/dukecordex/commandhandler/slashcommands/SlashCommandHandler.java
+++ src/main/java/io/github/maheevil/dukecordex/commandhandler/slashcommands/SlashCommandHandler.java
@@ -1,13 +1,17 @@
 package io.github.maheevil.dukecordex.commandhandler.slashcommands;
 
 import io.github.maheevil.dukecordex.DukeCordEx;
+import io.github.maheevil.dukecordex.commandhandler.Extension;
 import io.github.maheevil.dukecordex.commandhandler.annotations.SlashCommandArgField;
 import org.javacord.api.event.interaction.SlashCommandCreateEvent;
+import org.javacord.api.interaction.SlashCommandInteraction;
 import org.javacord.api.interaction.SlashCommandInteractionOption;
+import org.javacord.api.interaction.SlashCommandOption;
 import org.javacord.api.interaction.SlashCommandOptionType;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SlashCommandHandler {
@@ -33,45 +37,92 @@ public class SlashCommandHandler {
                 mainRunner.runConsumer(null,event);
                 //return;
             }else {
-                try{
-                    var innerClass = mainRunner.argsClass;
-                    var constructor = innerClass.getConstructor(commandInstance.extensionInstance.getClass());
-                    var argsInstance = constructor.newInstance(commandInstance.extensionInstance);
-                    //boolean encounteredError = false;
-
-                    for(Field field : mainRunner.filteredFieldList){
-                        var value = slashCommandInteraction.getOptionByName(field.getName());
-                        var annotation = field.getAnnotation(SlashCommandArgField.class);
-
-                        if(value.isEmpty() && annotation.required()){
-                            System.err.println("Command arg signature does not match the local signature");
-                            break;
-                        }else if(value.isEmpty()){
-                            field.set(argsInstance,null);
-                            continue;
-                        }
-
-                        var valueGet = getOptionValueAsObject(value.get(), annotation.type());
-                        if(field.getType().isAssignableFrom(valueGet.getClass())){
-                            field.set(argsInstance,valueGet);
-                        }else if(annotation.required()){
-                            System.err.println("One of the Arg field's type cannot assign the argument value");
-                            break;
-                        }else {
-                            System.err.println("Skipped assigning an value to an option field - the field type is not assignable to value type.");
-                            field.set(argsInstance,null);
-                        }
-                    }
-
-                    mainRunner.runConsumer(argsInstance,event);
-
-                } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
+                runCommandRunner(
+                        "main",
+                        commandInstance.baseBranchingCommands,
+                        firstOption,
+                        slashCommandInteraction,
+                        event,
+                        commandInstance.extensionInstance
+                );
             }
         }
     }
 
+    private static void runCommandRunner(
+            String commandRunnerName,
+            HashMap<String,SlashCommandRunner<?>> commandRunnerHashMap,
+            SlashCommandInteractionOption subFirstOption,
+            Object parentOptionProvider,
+            SlashCommandCreateEvent event,
+            Extension extension
+    ){
+        if(!commandRunnerHashMap.containsKey(commandRunnerName)){
+            System.err.println("Invalid command registration");
+            return;
+        }
+
+        var mainRunner = commandRunnerHashMap.get(commandRunnerName);
+
+        if(subFirstOption == null){
+            mainRunner.runConsumer(null,event);
+        }else {
+            try{
+                mainRunner.runConsumer(
+                        parseArgs(
+                                mainRunner,
+                                extension,
+                                parentOptionProvider,
+                                commandRunnerName.equals("main")
+                        ),
+                        event
+                );
+            } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static Object parseArgs(
+            SlashCommandRunner<?> mainRunner,
+            Extension extensionInstance,
+            Object optionProvider,
+            boolean baseCommand
+    )
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException
+    {
+        var innerClass = mainRunner.argsClass;
+        var constructor = innerClass.getConstructor(extensionInstance.getClass());
+        var argsInstance = constructor.newInstance(extensionInstance);
+        //boolean encounteredError = false;
+
+        for(Field field : mainRunner.filteredFieldList){
+            var value = baseCommand
+                    ? ((SlashCommandInteraction) optionProvider).getOptionByName(field.getName())
+                    : ((SlashCommandInteractionOption) optionProvider).getOptionByName(field.getName());
+            var annotation = field.getAnnotation(SlashCommandArgField.class);
+
+            if(value.isEmpty() && annotation.required()){
+                System.err.println("Command arg signature does not match the local signature");
+                break;
+            }else if(value.isEmpty()){
+                field.set(argsInstance,null);
+                continue;
+            }
+
+            var valueGet = getOptionValueAsObject(value.get(), annotation.type());
+            if(field.getType().isAssignableFrom(valueGet.getClass())){
+                field.set(argsInstance,valueGet);
+            }else if(annotation.required()){
+                System.err.println("One of the Arg field's type cannot assign the argument value");
+                break;
+            }else {
+                System.err.println("Skipped assigning an value to an option field - the field type is not assignable to value type.");
+                field.set(argsInstance,null);
+            }
+        }
+        return argsInstance;
+    }
     public static Object getOptionValueAsObject(SlashCommandInteractionOption value, SlashCommandOptionType type){
         return switch(type){
             case STRING -> value.getStringValue().orElseThrow();

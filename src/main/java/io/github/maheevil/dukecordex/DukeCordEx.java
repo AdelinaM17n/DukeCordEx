3666c9fe1e8da9688ca21e153c045db6eab4b2b3
Maheesha <maheeshasithum@gmail.com>
2022-06-25 16:52:28 +0530

    Getting ready to actually use this

--- src/main/java/io/github/maheevil/dukecordex/DukeCordEx.java
+++ src/main/java/io/github/maheevil/dukecordex/DukeCordEx.java
@@ -2,6 +2,7 @@ package io.github.maheevil.dukecordex;
 
 import io.github.maheevil.dukecordex.commandhandler.ChatCommandContainer;
 import io.github.maheevil.dukecordex.commandhandler.CommandHandler;
+import io.github.maheevil.dukecordex.commandhandler.Extension;
 import io.github.maheevil.dukecordex.commandhandler.slashcommands.SlashCommandHandler;
 import io.github.maheevil.dukecordex.commandhandler.slashcommands.SlashCommandRegisterer;
 import io.github.maheevil.dukecordex.commandhandler.slashcommands.SlashCommandEx;
@@ -9,28 +10,32 @@ import io.github.maheevil.dukecordex.test.BanCommand;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.function.Predicate;
 
 public class DukeCordEx{
     public static HashMap<String, ChatCommandContainer<?>> CommandMap = new HashMap<>();
     public static HashMap<String, SlashCommandEx> SlashCommandMap = new HashMap<>();
-    public static DiscordApi api;
     public static void main(String[] args){
-        Predicate<String> stringPredicate = x -> true;
         DiscordApi discordApi = new DiscordApiBuilder()
                 .setToken(args[0])
                 .login().join();
-       System.out.println("done");
-       api = discordApi;
-       init(discordApi);
-       SlashCommandRegisterer.pushAllSlashCommands(SlashCommandMap.values().stream().toList(),api);
+        init(
+                discordApi,
+                new BanCommand()
+        );
+
     }
 
-    public static void init(DiscordApi discordApi){
+    public static void init(DiscordApi discordApi, Extension... extensions){
         discordApi.addMessageCreateListener(CommandHandler::onMessageCreate);
+        SlashCommandRegisterer.pushAllSlashCommands(SlashCommandMap.values().stream().toList(),discordApi);
         discordApi.addSlashCommandCreateListener(SlashCommandHandler::handleSlashCommandEvent);
-        new BanCommand();
+        Arrays.stream(extensions).toList().forEach(
+                extension -> System.out.println(extension.getClass().getSimpleName() + " extension loaded!")
+        );
     }
     //TODO - Make the chat commands also viable in non-guild environments?
 }
